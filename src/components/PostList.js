2bbe1c6ce6a249d11ac9fbc43257809919ab2c73
Michael Matich <matich.mike@gmail.com>
2020-09-03 22:42:12 -0400

    additional refactor using lodash

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { fetchPosts } from '../actions';
+import { fetchPostsAndUsers } from '../actions';
 import UserHeader from './UserHeader';
 
 class PostList extends React.Component {
   componentDidMount() {
-    this.props.fetchPosts();
+    this.props.fetchPostsAndUsers();
   }
 
   renderList() {
     return this.props.posts.map(post => {
       return (
         <div className="item" key={post.id}>
-          <i className="large midle aligned icon user" />
+          <i className="large middle aligned icon user" />
           <div className="content">
             <div className="description">
               <h2>{post.title}</h2>
@@ -40,4 +40,4 @@ const mapStateToProps = state => {
   return { posts: state.posts }
 }
 
-export default connect(mapStateToProps, {fetchPosts})(PostList);
+export default connect(mapStateToProps, {fetchPostsAndUsers})(PostList);
