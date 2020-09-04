e7d0396979eb6ec56cd6cee2ce1a07d98ae30ff8
Michael Matich <matich.mike@gmail.com>
2020-09-03 21:57:17 -0400

    initial memoize added

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -8,12 +8,21 @@ class UserHeader extends React.Component {
   }
 
   render() {
+    const {user} = this.props;
+    if(!user) {
+      return null;
+    }
+
     return (
-      <div>
-        User Header
+      <div className="header">
+        {user.name}
       </div>
     )
   }
 }
 
-export default connect(null, { fetchUser })(UserHeader);
+const mapStateToProps = (state, ownProps) => {
+  return { user: state.users.find(user => user.id === ownProps.userId) }
+}
+
+export default connect(mapStateToProps, { fetchUser })(UserHeader);
